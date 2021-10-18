feb2ce16d651d424c131c5fee5af52fab7f7336f
Gibbu <Gibbu_@hotmail.com>
2021-10-18 12:50:14 +1100

    Updated repo

--- scripts/build.js
+++ scripts/build.js
@@ -1,33 +1,10 @@
-// Used to build scss files to the dist folder.
+// GitHub action used to build scss files to the dist folder.
+// You are not meant to run this locally.
 
-const sass = require('sass');
-const fs = require('fs');
-const path = require('path');
-const postcss = require('postcss');
-const autoprefixer = require('autoprefixer');
+const compile = require('./compile');
+const {name, build} = require('./config.json');
 
-console.log('Building iPadOS.css file...');
-
-sass.render({
-	file: 'src/_base.scss',
-	outputStyle: 'expanded'
-}, (err, result) => {
-	if (err) {
-		console.error(err);
-		return false;
-	}
-
-	const newRes = Buffer.from(result.css).toString();
-
-	postcss([autoprefixer])
-		.process(newRes, {
-			from: undefined,
-			to: undefined
-		})
-		.then(postcssRes => {
-			fs.writeFile(path.join('dist', 'iPadOS.css'), postcssRes.css, (err) => {
-				if (err) console.error(err);
-				else console.log(`Successfully built iPadOS.css file. (${(result.stats.duration/60000 * 60).toFixed(2)}s)`);
-			})
-		})
-})
+compile({
+	target: build.target,
+	output: [...build.outputPath, `${name}.css`]
+});
